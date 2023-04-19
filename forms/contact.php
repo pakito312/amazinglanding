5c3c06198c54c151b8de8c9590a310a0640ea838
pakito312 <pakileonel1@gmail.com>
2023-04-19 15:36:13 +0100

    push add

--- forms/contact.php
+++ forms/contact.php
@@ -9,33 +9,29 @@
   // Replace contact@example.com with your real receiving email address
   $receiving_email_address = 'info@amazingpayment.fr';
 
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
+  if( file_exists($php_email_form = 'class.phpmailer.php' )) {
     include( $php_email_form );
   } else {
     die( 'Unable to load the "PHP Email Form" Library!');
   }
 
-  $contact = new PHP_Email_Form;
-  $contact->ajax = true;
-  
-  $contact->to = $receiving_email_address;
-  $contact->from_name = $_POST['name'];
-  $contact->from_email = $_POST['email'];
-  $contact->subject = $_POST['subject'];
-
-  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
-  $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
-    'port' => '587'
-  );
-  */
-
-  $contact->add_message( $_POST['name'], 'From');
-  $contact->add_message( $_POST['email'], 'Email');
-  $contact->add_message( $_POST['message'], 'Message', 10);
-
-  echo $contact->send();
+  $contact = new PHPMailer(true);
+  $contact->CharSet = 'UTF-8';
+  $contact->isSMTP();
+  $contact->Host       = 'smtp.example.com';                     //Set the SMTP server to send through
+  $contact->SMTPAuth   = true;                                   //Enable SMTP authentication
+  $contact->Username   = 'user@example.com';                     //SMTP username
+  $contact->Password   = 'secret';                               //SMTP password
+  $contact->SMTPSecure = 'ssl';            //Enable implicit TLS encryption
+  $contact->Port       = 465; 
+  $contact->AddReplyTo($_POST['email'], $_POST['name']);
+  $contact->addAddress($receiving_email_address, 'Receiving Name');
+  $contact->Subject = $_POST['subject'];
+  $contact->Body = $_POST['message'];
+  $mail=$contact->send();
+  if($mail){
+    echo "ok";
+  }else{
+    echo "error";
+  }
 ?>
